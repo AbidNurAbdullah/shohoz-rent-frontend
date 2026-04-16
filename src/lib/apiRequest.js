448509d3611dd62573ef12dcadb1b0e9a7c812a0
AbidNurAbdullah <jasondrewla@gmail.com>
2026-04-16 23:57:46 +0600

    Updated apiRequest with credentials

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://shohoz-rent-backend.onrender.com/api",
+  // এটি লোকালহোস্ট এবং রেন্ডার উভয়ই হ্যান্ডেল করবে
+  baseURL: import.meta.env.VITE_API_URL || "https://shohoz-rent-backend.onrender.com/api",
   withCredentials: true,
 });
 
